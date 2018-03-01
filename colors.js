f30278e44a1531d145cf3a9c005231812431de98
Usoroh <paius.usoroh@gmail.com>
2018-03-01 11:59:37 +0600

    Update colors.js with new functionality
    Added pickColor(), generateColors(), and randomColor() functions

--- colors.js
+++ colors.js
@@ -1,31 +1,58 @@
 
-var colors = [
- "rgb(255, 0, 0)",
- "rgb(255, 255, 0)",
- "rgb(0, 255, 0)",
- "rgb(0, 255, 255)",
- "rgb(0, 0, 255)",
- "rgb(255, 0, 255)"
-]
+var colors = generateColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
-	
+
 	squares[i].addEventListener("click", function() {
 		//grab color of clicked square
 		var clickedColor = this.style.backgroundColor;
 		//compare color to picked color
 		if(clickedColor === pickedColor) {
-			alert("correct");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("wrong");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try again";
 		}
 	});
 	squares[i].style.backgroundColor = colors[i];
 }
 
+function changeColors(color) {
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateColors(num) {
+	//make an array
+	var arr =[];
+	//add num random colors to array
+	for(var i = 0; i < num; i++){
+		//get random color and push it into arr
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor() {
+	//pick red from 0 to 255
+	var r = Math.floor(Math.random() * 256);
+	//pick green from 0 to 255
+	var g = Math.floor(Math.random() * 256);		
+	// pick blue from 0 to 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
